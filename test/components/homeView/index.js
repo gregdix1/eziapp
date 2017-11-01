7beb3bd6878e908ce968461ddc75c54c90f76c2a
Greg Dix <greg@ididafrica.co.za>
2017-11-01 13:07:31 +0000

    Changes made by Greg Dix (greg@ididafrica.co.za).

---
+++ test/components/homeView/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+app.homeView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+app.localization.registerView('homeView');
+
+// START_CUSTOM_CODE_homeView
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+(function () {
+    app.homeView.set('title', 'Home');
+})();
+function setFrame(){
+ document.getElementById("sgframe").src = "components/contactsView/view.html";
+ document.getElementById("setdiv").innerHTML = "ccccccc";
+    alert('yes');}
+// END_CUSTOM_CODE_homeView
